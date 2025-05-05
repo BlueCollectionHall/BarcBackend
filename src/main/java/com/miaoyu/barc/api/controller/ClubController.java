fb378b6eb90cbc110787002288360cc55f994284
Yasenbaka <1260352143@qq.com>
2025-05-05 22:34:05 +0800

    club信息添加阻挠令牌拦截器

--- src/main/java/com/miaoyu/barc/api/controller/ClubController.java
+++ src/main/java/com/miaoyu/barc/api/controller/ClubController.java
@@ -1,6 +1,7 @@
 package com.miaoyu.barc.api.controller;
 
 import com.miaoyu.barc.annotation.ApiPath;
+import com.miaoyu.barc.annotation.IgnoreAuth;
 import com.miaoyu.barc.api.service.ClubService;
 import com.miaoyu.barc.utils.J;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,7 @@ public class ClubController {
 
     /**获取BA中全部的club实体
      * @return List类型中包含BA中全部的club实体*/
+    @IgnoreAuth
     @GetMapping("/clubs_all")
     public ResponseEntity<J> getBaAllClubControl() {
         return clubService.getAllClubsService();
@@ -25,6 +27,7 @@ public class ClubController {
     /**根据school_id获取BA中全部符合条件的club实体
      * @param schoolId school的id
      * @return List类型中包含BA中符合school_id条件的全部club实体*/
+    @IgnoreAuth
     @GetMapping("/clubs_by_school")
     public ResponseEntity<J> getBaClubsBySchoolControl(
             @RequestParam("school_id") String schoolId
@@ -35,6 +38,7 @@ public class ClubController {
     /**根据club_id获取符合条件的唯一club实体
      * @param clubId club的id
      * @return 符合条件的唯一club实体*/
+    @IgnoreAuth
     @GetMapping("/club_only")
     public ResponseEntity<J> getBaClubOnlyControl(
             @RequestParam("club_id") String clubId
